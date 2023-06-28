e9f82ac0d61dea3a3a151c58858f5c7af4a92443
Samim <mollahsamim4@gmail.com>
2023-06-28 19:41:15 +0530

    I have changes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hi <b>{{ userData.name }}</b> and your email is <b>{{ userData.email }}</b></h1>
+  <br/>
+<div>
+
+      <input type="text" name="" id="" v-model="userData.name" placeholder="Type your name here...">
+      <input type="email" name="" id="" v-model="userData.email" placeholder="Type your email here..">
+
+      <input type="text" name="" id="" v-model="userData.website" placeholder="Type your website here...">
+      </div>
+
+</template>
+
+
+<script setup="">
+import { reactive } from 'vue';
+
+
+const userData=reactive({
+  name:'',
+  email:'',
+  website:''
+})
+
+
+</script>
